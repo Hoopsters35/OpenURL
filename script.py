91ad8d38a68dafbcfe3a96312b48cb5e4a6f8e72
Justin Knueppel <justinknueppel@gmail.com>
2018-09-23 11:00:16 -0400

    Allow file path to be chosen

--- script.py
+++ script.py
@@ -3,8 +3,7 @@ import re, webbrowser
 # regex for URL
 regex = re.compile('(((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?)')
 
-# filepath = input('Path for input file: ')
-filepath = '/home/justin/school/ENGR2367/recRepNotes/reccomendationssources.txt'
+filepath = input('Path for input file: ')
 
 with open(filepath) as file:
 	links = regex.findall("".join(file.readlines()))
