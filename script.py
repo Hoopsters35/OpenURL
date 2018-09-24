1d286f04270f4c5c9904b75224589abc59e25cfa
Hoopsters35 <32350916+Hoopsters35@users.noreply.github.com>
2018-09-24 15:12:10 -0400

    allow ability to put text in command line args

--- script.py
+++ script.py
@@ -1,13 +1,18 @@
-import re, webbrowser
+import re, webbrowser, sys
 
 # regex for URL
 regex = re.compile('(((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?)')
 
-filepath = input('Path for input file: ')
+if len(sys.argv) == 1:
+	filepath = input('Path for input file: ')
 
-with open(filepath) as file:
-	links = regex.findall("".join(file.readlines()))
-	links = [link[0] for link in links]
+	with open(filepath) as file:
+		text = "".join(file.readlines())
 
+if len(sys.argv) > 1:
+	text = sys.argv[1]
+
+links = regex.findall(text)
+links = [link[0] for link in links]
 for url in links:
 	webbrowser.open(url)
